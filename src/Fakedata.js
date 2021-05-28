1a2475052287e1f64b6c2f26dfb73c14e0cdfdf3
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-05-28 20:49:59 +0600

    first commit

---
+++ src/Fakedata.js
@@ -0,0 +1,63 @@
+const Electronics=[{
+    "id":0,
+    "name": "AC",
+    "Image": "https://i.ibb.co/7R7y75X/ac.jpg",
+    "rent": 69
+  },{
+    "id":1,
+    "name": "Bulb",
+    "Image": "https://i.ibb.co/mTYFdC5/bulb.jpg",
+    "rent": 80
+  },{
+    "id":2,
+    "name": "Computer",
+    "Image": "https://i.ibb.co/PmsxJKM/computer-1205-717.jpg",
+    "rent": 45
+  },{
+    "id":3,
+    "name": "Fan",
+    "Image": "https://i.ibb.co/dL1rW47/fan.jpg",
+    "rent": 100
+  },{
+    "id":4,
+    "name": "Iphone",
+    "Image": "https://i.ibb.co/pQBqHX1/iphone.jpg",
+    "rent": 120
+  },{
+    "id":5,
+    "name": "KeyBoard",
+    "Image": "https://i.ibb.co/NKcCwS6/keyboard.jpg",
+    "rent": 200
+  },{
+    "id":6,
+    "name": "Laptop",
+    "Image": "https://i.ibb.co/MBswmkb/Laptop.jpg",
+    "rent": 300
+  },{
+    "id":7,
+    "name": "Mouse",
+    "Image": "https://i.ibb.co/KbgnSSd/mouse.jpg",
+    "rent": 69
+  },{
+    "id":8,
+    "name": "Refrigerator",
+    "Image": "https://i.ibb.co/T2jv5mj/refrigerator.jpg",
+    "rent": 450
+  },{
+    "id":9,
+    "name": "Remote",
+    "Image": "https://i.ibb.co/fH9mNh2/remote.jpg",
+    "rent": 50
+  },{
+    "id":10,
+    "name": "Router",
+    "Image": "https://i.ibb.co/p2Jzwz4/router.jpg",
+    "rent": 110
+  },{
+    "id":11,
+    "name": "AC",
+    "Image": "https://i.ibb.co/GdSvcxt/samsung-tv-mockup-1022-20.jpg",
+    "rent": 600
+  },
+]
+export default Electronics;
